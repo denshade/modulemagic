9bbf81b14d990e3eef24859790735056c7a3700e
denshade <lieven.vaneeckhaute@gmail.com>
2019-01-01 07:48:38 +0100

    minor edits

--- tabscript.py
+++ tabscript.py
@@ -1,53 +1,112 @@
 import sys
 import csv
 import os
-from subprocess import call
+import subprocess
 from shutil import copyfile
 
+
+def writeoutput(output : str):
+    f = open("current.csv", 'w')
+    f.write(output)
+    f.close()
 
 
 class resource:
     def __init__(self, filename):
         self.filename = filename
 
-class FileResource(resource):
-    def exec(self, other_resource: resource):
-        if type(other_resource) == FileResource:
-            print("Copied file from " + self.filename, other_resource.filename)
-            copyfile(self.filename, other_resource.filename)
-        if type(other_resource) == TProcessResource:
-            print("Watch out, trying to call a regular file with a table proc!")
-            if other_resource.filename.endswith(".py"):
-                call(["python", other_resource.filename, self.filename])
-            else:
-                call([other_resource.filename, self.filename])
-        if type(other_resource) == ProcessResource:
-            print("Calling " + other_resource.filename + " " + self.filename)
-            if other_resource.filename.endswith(".py"):
-                call(["python", other_resource.filename, self.filename])
-            else:
-                call([other_resource.filename, self.filename])
 
+def proc(myresource : resource, other_resource : resource):
+    print("Calling " + other_resource.filename, myresource.filename)
+    if other_resource.filename.endswith(".py"):
+        output = subprocess.checkoutput(["python", other_resource.filename, myresource.filename])
+    else:
+        output = subprocess.checkoutput([other_resource.filename, myresource.filename])
+    writeoutput(output)
+
+
+class FileResource(resource):
+    def exec(self, dest_resource: resource):
+        if type(dest_resource) == FileResource:
+            print("Copied file from " + self.filename, dest_resource.filename)
+            copyfile(self.filename, dest_resource.filename)
+        if type(dest_resource) == TProcessResource:
+            print("Watch out, trying to call a regular file with a table proc!")
+            if dest_resource.filename.endswith(".py"):
+                subprocess.checkoutput(["python", dest_resource.filename, self.filename])
+            else:
+                subprocess.checkoutput([dest_resource.filename, self.filename])
+        if type(dest_resource) == ProcessResource:
+            proc(self, dest_resource)
+
+
+def row_to_param(row):
+    str = ""
+    for key in row:
+        str += "--"+key+"="+row[key]
+    return str
 
 
 class TableResource(resource):
-    def exec(self, other_resource: resource):
-        if type(other_resource) == TProcessResource:
-            print("Calling " + other_resource.filename, self.filename)
-            if other_resource.filename.endswith(".py"):
-                call(["python", other_resource.filename, self.filename])
-            else:
-                call([other_resource.filename, self.filename])
+    def exec(self, dest_resource: resource):
+        if type(dest_resource) == FileResource or type(dest_resource) == TableResource:
+            print("Copied file table " + self.filename + " to " + dest_resource.filename)
+            copyfile(self.filename, dest_resource.filename)
+        if type(dest_resource) == TProcessResource:
+            proc(self, dest_resource)
+        if type(dest_resource) == ProcessResource:
+            reader = csv.DictReader(self.filename)
+            for row in reader:
+                print("Calling " + dest_resource.filename + row_to_param(row))
+                if dest_resource.filename.endswith(".py"):
+                    output = subprocess.checkoutput(["python", dest_resource.filename, row_to_param(row)])
+                else:
+                    output = subprocess.checkoutput([dest_resource.filename, row_to_param(row)])
+                writeoutput(output)
 
 
 class ProcessResource(resource):
-    def exec(self, other_resource: resource):
-        pass
+    def exec(self, dest_resource: resource):
+        #use current.csv
+        sourcefile = "current.csv"
+        if type(dest_resource) == FileResource:
+            print("Copied file from " + sourcefile + " " + dest_resource.filename)
+            copyfile(self.sourcefile, dest_resource.filename)
+        if type(dest_resource) == TProcessResource:
+            print("Watch out, trying to call a regular file with a table proc!")
+            if dest_resource.filename.endswith(".py"):
+                subprocess.checkoutput(["python", dest_resource.filename, sourcefile])
+            else:
+                subprocess.checkoutput([dest_resource.filename, sourcefile])
+        if type(dest_resource) == ProcessResource:
+            print("Calling " + dest_resource.filename + sourcefile)
+            if dest_resource.filename.endswith(".py"):
+                output = subprocess.checkoutput(["python", dest_resource.filename, sourcefile])
+            else:
+                output = subprocess.checkoutput([dest_resource.filename, sourcefile])
+            writeoutput(output)
+
 
 
 class TProcessResource(resource):
-    def exec(self, other_resource: resource):
-        pass
+    def exec(self, dest_resource: resource):
+        #use current.csv
+        sourcefile = "current.csv"
+        if type(dest_resource) == FileResource:
+            print("Copied file from " + sourcefile + " " + dest_resource.filename)
+            copyfile(self.sourcefile, dest_resource.filename)
+        if type(dest_resource) == TProcessResource:
+            if dest_resource.filename.endswith(".py"):
+                subprocess.checkoutput(["python", dest_resource.filename, sourcefile])
+            else:
+                subprocess.checkoutput([dest_resource.filename, sourcefile])
+        if type(dest_resource) == ProcessResource:
+            print("Calling " + dest_resource.filename + sourcefile)
+            if dest_resource.filename.endswith(".py"):
+                output = subprocess.checkoutput(["python", dest_resource.filename, sourcefile])
+            else:
+                output = subprocess.checkoutput([dest_resource.filename, sourcefile])
+            writeoutput(output)
 
 
 def parseresource(resource: str) -> resource:
@@ -71,10 +130,10 @@ def parseresource(resource: str) -> resource:
 
 # from file to file = copy file to file.
 # from file to table = impossible
-# from file to process = pipe data from file to process as input.
-# from file to tprocess = impossible.
+# from file to process = feed file as parameter to process.
+# from file to tprocess = impossible. Warn.
 
-# from table to file = stores the tsv to a file.
+# from table to file = stores the csv to a file.
 # from table to table = copy
 # from table to process = for each row in the process
 # from table to tprocess = process gets the file.
@@ -95,6 +154,7 @@ def execute_resource_chain(resource_chain):
         from_chain_resource = resource_chain[i]
         to_chain_resource = resource_chain[i + 1]
         from_chain_resource.exec(to_chain_resource)
+
 
 def parseline(line: str):
     if line == "":
