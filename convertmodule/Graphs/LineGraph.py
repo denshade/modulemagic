985f0ee65bb813f03a56650d9c8da767eb147356
denshade <lieven.vaneeckhaute@gmail.com>
2019-01-24 22:28:07 +0100

    line graph

--- convertmodule/Graphs/LineGraph.py
+++ convertmodule/Graphs/LineGraph.py
@@ -1,2 +1,37 @@
+import csv
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
 __author__ = 'lveeckha'
+
+if len(sys.argv) != 2 or sys.argv[1] == "--inputparameters":
+    print("Input file. Expected is the first column as the label, the other columns are y values.")
+    sys.exit(2)
+if sys.argv[1] == "--validate":
+    pass
+    #validate(sys.argv[2])
+
+
+
+input_file = sys.argv[1]
+
+tick = 0
+y_array = []
+my_xticks = []
+with open(input_file) as f:
+    lines = f.readlines()
+    for line in lines[1:]:
+        tick += 1
+        y_array.append(float(line.split(",")[1]))
+        my_xticks.append(line.split(",")[0])
+
+
+
+x = np.array(range(1, len(lines)))
+y = np.array(y_array)
+plt.xticks(x, my_xticks, rotation='vertical')
+plt.locator_params(nbins=4)
+plt.plot(x, y)
+plt.show()
 
